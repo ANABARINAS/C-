3ab7f94d21ef74572812c24f60744800ce8a732d
anaibarinas <anaibarinas@gmail.com>
2023-02-03 22:07:51 -0500

    for

---
+++ C#/For/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace For
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            string[] amigos = new string[7]
+            {
+                "Pepe",
+                "Luisa",
+                "Mia",
+                "Lucía",
+                "Lucas",
+                "Martín",
+                "Rosa",
+            };
+
+            for (int i=0; i<amigos.Length; i++)
+            {
+                Console.WriteLine(amigos[i]);
+            }
+        }
+    }
+}
